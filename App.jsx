3df83d52ddc54cefb32f04df0a2c98cb6699d86c
dongalvinia <dongalvinia86@gmail.com>
2025-08-05 22:01:39 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/galaxy.png" className="galaxy" alt="Galaxy" />
+    </div>
+  );
+}
+
+export default App;
